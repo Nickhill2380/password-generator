fc1680bdd86f096e7e3419e9edcacb7ff1749065
Nick Hill <nickhill2380@gmail.com>
2020-10-29 21:43:55 -0400

    started the password length and criteria prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,51 @@
 // Assignment code here
 
+//prompt user for password criteria
+
+//prompt user for length of password between 8 and 128 characters
+var passwordLength = function() {
+
+  var characterLength = window.prompt("How long does you password need to be?");
+
+  characterLength = parseInt(characterLength);
+
+if (characterLength >= 8 && characterLength <= 128) {
+   return characterLength;
+} else {
+  window.alert("Please select a correct value.");
+  passwordLength();
+  
+};
+};
+
+//prompt character types to include lowercase, uppercase, numeric, and/or special characters
+var criteria = function() {
+var lowercase = window.confirm("Does your password need to include lowercase letters?");
+
+    if (lowercase){
+      return lowercase;
+    }
+var uppercase = window.confirm("Does your password need to include uppercase letters?");
+    if (uppercase){
+      return uppercase;
+    }
+
+var numeric = window.confirm("Does your password need to include numbers?");
+    if (numeric) {
+      return numeric;
+    }
+
+var special = window.confirm("Does your password need to include special characters?");
+    if(special) {
+      return special;
+    }
+
+    if (!special && !numeric && !uppercase && !lowercase) {
+      window.alert("You must select some criteria to use.");
+      criteria();
+    };
+  };
+//rerun if user does not select anything
 
 
 
@@ -13,10 +59,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+passwordLength();
+criteria();
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password Generator Starter Code
+
+## Description
+
+I was tasked with created a password generator that will allow the user to select the criteria they need to meet and then it will generate a password for them. I think I can do this using prompts, switch and if conditions in javascript. 
