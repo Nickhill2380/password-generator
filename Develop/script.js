cd2c0f7a5b7554200ac8cb54b341e414f32281ab
Nick Hill <nickhill2380@gmail.com>
2020-10-30 09:54:39 -0400

    got copy button working, cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -7,20 +7,22 @@ var passwordLength = function() {
   
   var characterLength = window.prompt("How long does you password need to be?");
   
+  
 
   characterLength = parseInt(characterLength);
-
-if (characterLength >= 8 && characterLength <= 128) {
-  console.log(characterLength);
+  
+  if (characterLength >= 8 && characterLength <= 128) {
+  
+  
    
   charLength = characterLength;
   
-   
 } else {
   window.alert("Please select a correct value.");
   passwordLength();
   
-};
+} 
+  
 return charLength;
 };
 
@@ -32,28 +34,28 @@ var lowercase = window.confirm("Does your password need to include lowercase let
     
     if(lowercase){
       criteriaSet = criteriaSet + "abcdefghijklmnopqrstuvwxyz";
-      console.log(criteriaSet);
+      
     };
 
 var uppercase = window.confirm("Does your password need to include uppercase letters?");
 
     if (uppercase) {
       criteriaSet = criteriaSet +"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      console.log(criteriaSet);
+      
     };
   
 var numeric = window.confirm("Does your password need to include numbers?");
    
     if (numeric) {
       criteriaSet = criteriaSet + "0123456789";
-      console.log(criteriaSet);
+      
 };
 
 var special = window.confirm("Does your password need to include special characters?");
 
     if (special) {
       criteriaSet = criteriaSet + "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-      console.log(criteriaSet);
+      
     }
    
 
@@ -70,11 +72,13 @@ var generatePassword = function() {
   
   password = "";
 
+
+
   for ( var i = 0; i < charLength; i++ ){
     
     password += criteriaSet.charAt(Math.floor(Math.random() * criteriaSet.length));
 
-    console.log(password);
+    
   }
   
   return password;
@@ -87,6 +91,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  passwordLength();
+  criteria();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -95,11 +101,20 @@ function writePassword() {
 
 }
 
+var copyBtn = document.getElementById("password");
+
+function copyPassword() {
+  
+  copyBtn.select(password);
+  copyBtn.setSelectionRange(0,9999);
+  document.execCommand("copy");
+
+  window.alert("Copied the password.");
+}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+copyBtn.addEventListener("click", copyPassword);
 
-passwordLength();
-criteria();
--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 ## Description
 
-I was tasked with created a password generator that will allow the user to select the criteria they need to meet and then it will generate a password for them. I think I can do this using prompts, switch and if conditions in javascript. 
+I was tasked with created a password generator that will allow the user to select the criteria they need to meet and then it will generate a password for them. I used a series of prompt to determine what style of password the user needed. The prompt appear after the generate button is clicked. The first prompt as for a password length which is then used as a value to determine the amount of times the for loop is run in the password function. The next four prompts will ask the user what kind of characters they can use in their password. If the user selects none of them they are informed they must choose one and the prompts start over. Once the prompts are completed the generate password functions goes through the for loop, the number of times determined by the user and selected one character each time from the criteria set by the user. Then can then click the copy password button and be alerted the password has been copied the user can then paste the password, where they need it. 
+
+    Going forward to improve on the functionality of this website. I'm going to look into a way to ensure the for loop selects one of each of the criteria selected. In case the user needs to use them and not just wants to. This would be used to cut out rare case where do to random propability, certain are selected by user but not by the function, causing the user to have to redo go through the prompts to get a password they can use.
+
+## Usage
+
+
+## Credits
+
+The website was designed for the class. I set up the characterLength, criteriaSet, generatePassword, copyPassword and the copyBtn functions in javascript. I also put the copy button in the html file. However, used the existing tags to keep its style the same as the generate password button.  I used the following webpages to help with my generator.
+
+I used this for idea of how to get the for loop to pull from the criteriaSet
+
+https://stackoverflow.com/questions/1497481/javascript-password-generator
+
+I used this to help get my copy button to work:
+
+https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+
